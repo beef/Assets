923e9a5e4233030a89ed7993dc667251470ad895
Steve England <steve@wearebeef.co.uk>
2009-06-18 16:02:30 +0100

    Fixing flash session stuff for swfupload

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,10 @@
-require 'admin_area'
+require 'flash_sesion_cookie_middleware'
+config.to_prepare do
+  # Rack middleware for fixing flash's issues with sessions
+  # http://thewebfellas.com/blog/2008/12/22/flash-uploaders-rails-cookie-based-sessions-and-csrf-rack-middleware-to-the-rescue
+  ActionController::Dispatcher.middleware.use FlashSessionCookieMiddleware, ActionController::Base.session_options[:key]
+end
 
+require 'admin_area'
 ActiveRecord::Base.send :include, Beef::Has::Assets
+
