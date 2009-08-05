c117b13f7cf413fa4fa7044b1cd56125e7aa360a
Steve England <steve@wearebeef.co.uk>
2009-08-05 19:34:29 +0100

    Modifying helpers to search array if a no scopable array is not passed in. Helps a lot with previews

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,12 +1,26 @@
 module AssetsHelper
 
   def gallery(assets, size = :square)
-    images_items = assets.images.collect { |asset| content_tag( :li, link_to(image_tag(asset.public_filename(size), :alt => asset.description), asset.public_filename, :title => asset.description, :rel => 'gallery' ) ) }
+    if assets.respond_to? :images and assets.images.any?
+      images = assets.images
+    elsif assets.any?
+      images = assets.reject{|a| !a.content_type.starts_with?('image')}
+    else
+      return
+    end
+    images_items = images.collect { |asset| content_tag( :li, link_to(image_tag(asset.public_filename(size), :alt => asset.description), asset.public_filename, :title => asset.description, :rel => 'gallery' ) ) }
     content_tag :ul, images_items.join, :class => 'gallery' unless images_items.empty?
   end
   
   def documents(assets)
-    documents_items = assets.documents.collect { |asset| content_tag( :li, link_to("Download #{asset.filename} (#{number_to_human_size(asset.size)})", asset.public_filename, :title => asset.description ) ) }
+    if assets.respond_to? :documents and assets.documents.any?
+      documents = assets.documents
+    elsif assets.any?
+      documents = assets.reject{|a| a.content_type.starts_with?('image')}
+    else
+      return
+    end
+    documents_items = documents.collect { |asset| content_tag( :li, link_to("Download #{asset.filename} (#{number_to_human_size(asset.size)})", asset.public_filename, :title => asset.description ) ) }
     content_tag :ul, documents_items.join, :class => 'documents' unless documents_items.empty?
   end
 end
