2547ce2c9d0e9948ef577307384781dee843f560
Steve England <steve@wearebeef.co.uk>
2010-01-19 18:45:38 +0000

    Adding span around filename to aid styling

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -11,7 +11,7 @@ module AssetsHelper
     images_items = images.collect { |asset| content_tag( :li, link_to(image_tag(asset.public_filename(size), :alt => asset.description), asset.public_filename, :title => asset.description, :rel => 'gallery' ) ) }
     content_tag :ul, images_items.join, :class => 'gallery' unless images_items.empty?
   end
-  
+
   def documents(assets)
     if assets.respond_to? :documents and assets.documents.any?
       documents = assets.documents
@@ -20,7 +20,7 @@ module AssetsHelper
     else
       return
     end
-    documents_items = documents.collect { |asset| content_tag( :li, link_to("<em>Download #{asset.filename}</em> (#{number_to_human_size(asset.size)})", asset.public_filename, :title => asset.description ) ) }
+    documents_items = documents.collect { |asset| content_tag( :li, link_to("<em>Download <span class=\"filename\">#{asset.filename}</span></em> (#{number_to_human_size(asset.size)})", asset.public_filename, :title => asset.description ) ) }
     content_tag :ul, documents_items.join, :class => 'documents' unless documents_items.empty?
   end
 end
